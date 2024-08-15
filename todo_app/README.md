374a44f0317250ba2bd803e2d98c923ff5db084b
Naeem Ahmed <naeemahmedbdn2002@gmail.com>
2024-08-15 17:33:30 +0500

    Update readme

--- todo_app/README.md
+++ todo_app/README.md
@@ -1,16 +1,65 @@
-# todo_app
+# ToDo App
 
-A new Flutter project.
+Welcome to the ToDo App! This application allows users to manage their tasks efficiently with a clean and intuitive interface.
 
-## Getting Started
+## Features
 
-This project is a starting point for a Flutter application.
+- Add, edit, and delete tasks
+- Mark tasks as completed
+- Add task description
 
-A few resources to get you started if this is your first Flutter project:
+## Screenshots
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+![Home Screen](results/home.png)
+*Home Screen*
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+![Add Task Screen](results/create_task.png)
+*Add Task Screen*
+
+![Task Details Screen](results/task_detail.png)
+*Task Details Screen*
+
+![Task Operations](results/delete_edit.png)
+*Task Operations Screen*
+
+![Task Update Screen](results/update.png)
+*Task Update Screen*
+
+## Installation
+
+1. Clone the repository:
+    ```bash
+    git clone https://github.com/yourusername/todo-app.git
+    ```
+
+2. Navigate into the project directory:
+    ```bash
+    cd todo-app
+    ```
+
+3. Install dependencies:
+    ```bash
+    flutter pub get
+    ```
+
+4. Run the app:
+    ```bash
+    flutter run
+    ```
+
+## Configuration
+
+Make sure you have Flutter and Dart installed on your machine. Follow the [official installation guide](https://flutter.dev/docs/get-started/install) if you haven't set up your environment yet.
+
+## Usage
+
+- **Add a Task**: Tap the "+" button to add a new task.
+- **Edit a Task**: Tap on a task to edit its details.
+- **Delete a Task**: Swipe left on a task to delete it.
+- **Mark as Completed**: Tap the checkbox next to a task to mark it as completed.
+
+
+## Acknowledgments
+
+- [Flutter](https://flutter.dev/) - Framework used for development.
+
--- todo_app/lib/screens/edit_task.dart
+++ todo_app/lib/screens/edit_task.dart
@@ -1,144 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:provider/provider.dart';
-// import 'package:todo_app/task_models/save_todo_provider.dart';
-// import 'package:todo_app/task_models/task_model.dart';
-// import 'package:todo_app/widgets/custom_app_bar.dart';
-
-// class EditTask extends StatefulWidget {
-//   final ToDo task;
-//   final int index;
-//   const EditTask({super.key, required this.index, required this.task});
-
-//   @override
-//   State<EditTask> createState() => _EditTaskState();
-// }
-
-// class _EditTaskState extends State<EditTask> {
-//   late TextEditingController _taskController, _taskDescController;
-
-//   @override
-//   void initState() {
-//     // TODO: implement initState
-//     super.initState();
-//     _taskController = TextEditingController(text: widget.task.taskName);
-//     _taskDescController = TextEditingController(text: widget.task.description);
-//   }
-//   @override
-//   Widget build(BuildContext context) {
-//     final UpdatetaskProvider = Provider.of<SaveTodoProvider>(context, listen: false);
-
-//     return SafeArea(
-//       child: Scaffold(
-//         backgroundColor: Colors.grey[100],
-//         body: SingleChildScrollView(
-//           child: Padding(
-//             padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
-//             child: Column(
-//               crossAxisAlignment: CrossAxisAlignment.start,
-//               children: [
-//                 const CustomAppBar(),
-//                 const SizedBox(height: 30),
-//                 const Text(
-//                   "Edit Task",
-//                   style: TextStyle(
-//                     fontSize: 24,
-//                     fontWeight: FontWeight.bold,
-//                     color: Colors.purple,
-//                   ),
-//                 ),
-//                 const SizedBox(height: 20),
-//                 Container(
-//                   decoration: BoxDecoration(
-//                     color: Colors.white,
-//                     borderRadius: BorderRadius.circular(10),
-//                     boxShadow: const [
-//                       BoxShadow(
-//                         color: Colors.black12,
-//                         blurRadius: 10,
-//                         offset: Offset(0, 5),
-//                       ),
-//                     ],
-//                   ),
-//                   child: TextFormField(
-//                     controller: _taskController,
-//                     maxLines: 1,
-//                     decoration: const InputDecoration(
-//                       hintText: "Title",
-//                       hintStyle: TextStyle(fontSize: 16),
-//                       border: InputBorder.none,
-//                       contentPadding: EdgeInsets.all(15),
-//                     ),
-//                   ),
-//                 ),
-//                 const SizedBox(height: 15),
-//                 Container(
-//                   decoration: BoxDecoration(
-//                     color: Colors.white,
-//                     borderRadius: BorderRadius.circular(10),
-//                     boxShadow: const [
-//                       BoxShadow(
-//                         color: Colors.black12,
-//                         blurRadius: 10,
-//                         offset: Offset(0, 5),
-//                       ),
-//                     ],
-//                   ),
-//                   child: TextFormField(
-//                     controller: _taskDescController,
-//                     maxLines: 5,
-//                     decoration: InputDecoration(
-//                       hintText: "Update notes",
-//                       hintStyle: TextStyle(
-//                         fontSize: 14,
-//                         fontWeight: FontWeight.normal,
-//                         color: Colors.grey[600],
-//                       ),
-//                       border: InputBorder.none,
-//                       contentPadding: const EdgeInsets.all(15),
-//                     ),
-//                   ),
-//                 ),
-//                 const SizedBox(height: 40),
-//                 Center(
-//                   child: ElevatedButton(
-//                     onPressed: () {
-//                       UpdatetaskProvider.updateTask(widget.index, _taskController.text, _taskDescController.text);
-//                       Navigator.pop(context);
-//                     },
-//                     style: ElevatedButton.styleFrom(
-//                       foregroundColor: Colors.white,
-//                       backgroundColor: Colors.purple,
-//                       shape: RoundedRectangleBorder(
-//                         borderRadius: BorderRadius.circular(5),
-//                       ),
-//                       padding: const EdgeInsets.symmetric(horizontal: 60, vertical: 15),
-//                       shadowColor: Colors.purpleAccent,
-//                       elevation: 5,
-//                     ),
-//                     child: const Text(
-//                       "Update Task",
-//                       style: TextStyle(
-//                         fontSize: 16,
-//                         fontWeight: FontWeight.bold,
-//                       ),
-//                     ),
-//                   ),
-//                 ),
-//               ],
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-
-
-
-
-
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_app/task_models/save_todo_provider.dart';
@@ -155,19 +14,18 @@ class EditTask extends StatefulWidget {
 }
 
 class _EditTaskState extends State<EditTask> {
-  late TextEditingController _taskController;
-  late TextEditingController _taskDescController;
+  late TextEditingController _taskController, _taskDescController;
 
   @override
   void initState() {
+    // TODO: implement initState
     super.initState();
     _taskController = TextEditingController(text: widget.task.taskName);
     _taskDescController = TextEditingController(text: widget.task.description);
   }
-
   @override
   Widget build(BuildContext context) {
-    final updateTaskProvider = Provider.of<SaveTodoProvider>(context, listen: false);
+    final UpdatetaskProvider = Provider.of<SaveTodoProvider>(context, listen: false);
 
     return SafeArea(
       child: Scaffold(
@@ -227,7 +85,7 @@ class _EditTaskState extends State<EditTask> {
                   ),
                   child: TextFormField(
                     controller: _taskDescController,
-                    maxLines: 8,
+                    maxLines: 5,
                     decoration: InputDecoration(
                       hintText: "Update notes",
                       hintStyle: TextStyle(
@@ -244,11 +102,7 @@ class _EditTaskState extends State<EditTask> {
                 Center(
                   child: ElevatedButton(
                     onPressed: () {
-                      updateTaskProvider.updateTask(
-                        widget.index,
-                        _taskController.text,
-                        _taskDescController.text,
-                      );
+                      UpdatetaskProvider.updateTask(widget.index, _taskController.text, _taskDescController.text);
                       Navigator.pop(context);
                     },
                     style: ElevatedButton.styleFrom(
@@ -278,3 +132,7 @@ class _EditTaskState extends State<EditTask> {
     );
   }
 }
+
+
+
+
